04ada5ebbe2a8792a2682507c1d5a924c77efe6c
Constantine Philipchenko <const-phi@ya.ru>
2018-03-13 23:14:18 +0300

    Add unsafe version of a quick sort algorithm. To complile use /unsafe flag or set it in project properties. Also add UNSAFE  text preprocessor constant.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace QuickSortDemo
 {
-    class Program
+    internal class Program
     {
         private static readonly Random random = new Random((Int32)DateTime.Now.Ticks);
 
@@ -47,12 +47,72 @@ namespace QuickSortDemo
             QuickSortBody(array, last + 1, finish);
         }
 
+#if UNSAFE
+        private static unsafe Double* UnsafeGenerate(Int32 size, Double min = Double.MinValue, Double max = Double.MaxValue)
+        {
+            fixed (Double* ptr = Generate(size, min, max))
+            {
+                return ptr;
+            }
+        }
+
+        private static unsafe void UnsafePrint(Double* array, Int32 size, String message)
+        {
+            if (size <= 0)
+                return;
+
+            Console.WriteLine($"\n{message}");
+            for (var i = 0; i < size; ++i)
+                Console.WriteLine($"\t{array[i]}");
+            Console.WriteLine();
+        }
+
+        private static unsafe void UnsafeSwap(Double* array, Int32 i, Int32 j)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+
+        private static unsafe void UnsafeQuickSort(Double* array, Int32 size)
+        {
+            if (size < 2)
+                return;
+
+            var pivot = random.Next(size);
+            UnsafeSwap(array, 0, pivot);
+            var last = 0;
+            for (var index = 0; index < size; ++index)
+                if (array[index] < array[0])
+                    UnsafeSwap(array, ++last, index);
+            UnsafeSwap(array, 0, last);
+            UnsafeQuickSort(array, last);
+            UnsafeQuickSort(array + last + 1, size - last - 1);
+        }
+#endif
+
         private static void Main()
         {
-            var array = Generate(size: 5, min: -10, max: 10);
+            const Int32 size = 5;
+            const Double min = -10;
+            const Double max = 10;
+
+#if UNSAFE
+            unsafe
+            {
+                var ptr = UnsafeGenerate(size, min, max);
+                UnsafePrint(ptr, size, "before:");
+                UnsafeQuickSort(ptr, size);
+                UnsafePrint(ptr, size, "after:");
+                level--;
+            }
+#else
+            var array = Generate(size, min, max);
             Print(array, "before:");
             QuickSort(array);
             Print(array, "after:");
+#endif
         }
     }
 }
